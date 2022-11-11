5b24cd5a03e4b93b6d3a233f71efd93b92f80b89
REETNY <shamsideenajide@gmail.com>
2022-11-11 12:15:52 +0100

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,23 @@
 const text = "\n Hey \n I'm Shamsideen \n Ajide";
 const textbox = document.getElementById("textBox");
+const rotate = document.getElementById("rotate");
+
 
 console.log(text);
 
 let index = 0;
 
 function writeText(){
-    textbox.innerText = text.slice(0, index);
-
-    index++;
+    if(index <= text.length){
+        textbox.innerText = text.slice(0, index);
+        index++;
+    }
 }
 
 setInterval(writeText, 100);
+
+rotate.addEventListener('click', rotateNav);
+
+function rotateNav(){
+    rotate.classList.toggle("clicked");
+}
